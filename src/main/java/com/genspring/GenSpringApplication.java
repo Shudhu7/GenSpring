b396d4caae6189c9b7aa6d65947b40c174ac7618
Shudhu7 <mr.shudhuingle@gmail.com>
2025-08-05 19:31:06 +0530

    initial

---
+++ src/main/java/com/genspring/GenSpringApplication.java
@@ -0,0 +1,13 @@
+package com.genspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genspring/GenSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
